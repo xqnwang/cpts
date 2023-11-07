05c880c8833394d689260cb2b3a3624fb5060ef8
xqnwang <wang_study1995@163.com>
2023-11-08 01:25:25 +1100

    Base function to calculate weighted quantile

---
+++ R/base.R
@@ -0,0 +1,7 @@
+weighted.quantile <- function(v, prob, w = NULL, sorted = FALSE) {
+  if (is.null(w)) w <- rep(1, length(v))
+  if (!sorted) { o <- order(v); v <- v[o]; w <- w[o] }
+  i <- which(cumsum(w/sum(w)) >= prob)
+  if (length(i) == 0) return(Inf) # Can happen with infinite weights
+  else return(v[min(i)])
+}
