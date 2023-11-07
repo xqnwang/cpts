d7ca1053c08b77de604ce1f92ae1f75b1ddd3ce7
xqnwang <wang_study1995@163.com>
2023-11-08 01:28:01 +1100

    Remove base functions

--- R/ICP_split.R
+++ R/ICP_split.R
@@ -45,11 +45,3 @@ ICP.split <- function(data, x0, alpha = 0.1, rho = 0.5, split = NULL, w = NULL) 
   
   return(list(pred = pred, lo = lo, up = up, split = i1))
 }
-
-weighted.quantile <- function(v, prob, w = NULL, sorted = FALSE) {
-  if (is.null(w)) w <- rep(1, length(v))
-  if (!sorted) { o <- order(v); v <- v[o]; w <- w[o] }
-  i <- which(cumsum(w/sum(w)) >= prob)
-  if (length(i) == 0) return(Inf) # Can happen with infinite weights
-  else return(v[min(i)])
-}
