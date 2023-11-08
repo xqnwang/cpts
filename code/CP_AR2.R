99f64c29057fd4b4087f5a92540430744ef3b7ac
xqnwang <wang_study1995@163.com>
2023-11-09 01:01:11 +1100

    Include results of AR(2) simulation

--- code/CP_AR2.R
+++ code/CP_AR2.R
@@ -6,12 +6,13 @@ source("R/ICP_update.R")
 
 # Simulation series AR(2)
 set.seed(0)
-series <- arima.sim(n = 5000, list(ar = c(0.8897, -0.4858)), sd = sqrt(0.2))
+series <- arima.sim(n = 5000, list(ar = c(0.8, -0.5)), sd = sqrt(1))
 autoplot(series) +
   ggtitle("AR(2) process") +
   xlab("Time") +
   ylab("") +
   theme_bw()
+save(series, file = "data/AR2data.rda")
 series <- as.numeric(series)
 
 #------------------------------------------------------------
@@ -70,7 +71,7 @@ len.split$NexCP <- out.NexCP$up - out.NexCP$lo
 save(cov.split, len.split, file = "data/AR2_split.rda")
 
 #------------------------------------------------------------
-# ICP with sliding training and calibration sets
+# ICP with rolling training and calibration sets
 #------------------------------------------------------------
 nfit <- 500
 ncal <- 500
@@ -150,46 +151,39 @@ len.split.median.df <- pivot_longer(len.split.median, cols = 2:5)
 lcl.split <- ggplot(data = cov.split.mean.df, aes(x = x, y = value, colour = name)) +
   geom_line() +
   geom_hline(yintercept=apply(cov.split, 2, mean), 
-             linetype='dashed', 
-             color=c('blue', 'yellow', 'orange', 'green')) +
+             linetype='dashed', color=c('blue', 'yellow', 'orange', 'green')) +
   scale_color_manual(
-    values = c(
-      CP="blue",
-      WCP.LR="yellow",
-      WCP.RF="orange",
-      NexCP="green")) +
+    values = c(CP="blue", WCP.LR="yellow", WCP.RF="orange", NexCP="green"),
+    breaks=c('CP', 'WCP.LR', 'WCP.RF', 'NexCP')) +
   labs(title = "Local Coverage Level",
        x = "Time", y = "",
        fill = "Method") +
-  theme(legend.position = "bottom")
-  
+  theme(legend.position = "bottom") +
+  theme_bw()
 
 mw.split <- ggplot(data = len.split.mean.df, aes(x = x, y = value, colour = name)) +
   geom_line() +
   scale_color_manual(
-    values = c(
-      CP="blue",
-      WCP.LR="yellow",
-      WCP.RF="orange",
-      NexCP="green")) +
+    values = c(CP="blue", WCP.LR="yellow", WCP.RF="orange", NexCP="green"),
+    breaks=c('CP', 'WCP.LR', 'WCP.RF', 'NexCP')) +
   labs(title = "Mean Width",
        x = "Time", y = "",
        fill = "Method") +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  theme_bw()
 
 mdw.split <- ggplot(data = len.split.median.df, aes(x = x, y = value, colour = name)) +
   geom_line() +
   scale_color_manual(
-    values = c(
-      CP="blue",
-      WCP.LR="yellow",
-      WCP.RF="orange",
-      NexCP="green")) +
+    values = c(CP="blue", WCP.LR="yellow", WCP.RF="orange", NexCP="green"),
+    breaks=c('CP', 'WCP.LR', 'WCP.RF', 'NexCP')) +
   labs(title = "Median Width",
        x = "Time", y = "",
        fill = "Method") +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  theme_bw()
 
+save(lcl.split, mw.split, mdw.split, file = "data/AR2_split_plot.rda")
 ggarrange(lcl.split, mw.split, mdw.split,
           ncol = 1, nrow = 3,
           common.legend = TRUE, legend = "bottom")
@@ -212,45 +206,39 @@ len.update.median.df <- pivot_longer(len.update.median, cols = 2:5)
 lcl.update <- ggplot(data = cov.update.mean.df, aes(x = x, y = value, colour = name)) +
   geom_line() +
   geom_hline(yintercept=apply(cov.update, 2, mean), 
-             linetype='dashed', 
-             color=c('gray', 'blue', 'green', 'red')) +
+             linetype='dashed', color=c('gray', 'blue', 'green', 'red')) +
   scale_color_manual(
-    values = c(
-      AR="gray",
-      CP="blue",
-      NexCP="green",
-      ACP="red")) +
+    values = c(AR="gray", CP="blue", NexCP="green", ACP="red"),
+    breaks=c('AR', 'CP', 'NexCP', 'ACP')) +
   labs(title = "Local Coverage Level",
        x = "Time", y = "",
        fill = "Method") +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  theme_bw()
 
 mw.update <- ggplot(data = len.update.mean.df, aes(x = x, y = value, colour = name)) +
   geom_line() +
   scale_color_manual(
-    values = c(
-      AR="gray",
-      CP="blue",
-      NexCP="green",
-      ACP="red")) +
+    values = c(AR="gray", CP="blue", NexCP="green", ACP="red"),
+    breaks=c('AR', 'CP', 'NexCP', 'ACP')) +
   labs(title = "Mean Width",
        x = "Time", y = "",
        fill = "Method") +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  theme_bw()
 
 mdw.update <- ggplot(data = len.update.median.df, aes(x = x, y = value, colour = name)) +
   geom_line() +
   scale_color_manual(
-    values = c(
-      AR="gray",
-      CP="blue",
-      NexCP="green",
-      ACP="red")) +
+    values = c(AR="gray", CP="blue", NexCP="green", ACP="red"),
+    breaks=c('AR', 'CP', 'NexCP', 'ACP')) +
   labs(title = "Median Width",
        x = "Time", y = "",
        fill = "Method") +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  theme_bw()
 
+save(lcl.update, mw.update, mdw.update, file = "data/AR2_update_plot.rda")
 ggarrange(lcl.update, mw.update, mdw.update,
           ncol = 1, nrow = 3,
           common.legend = TRUE, legend = "bottom")
