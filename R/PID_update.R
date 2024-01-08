d00862809e9c9b82186b6f9b6065c0c414a1da6a
xqnwang <wang_study1995@163.com>
2024-01-08 23:47:31 +1100

    Fix errors in PID

--- R/PID_update.R
+++ R/PID_update.R
@@ -6,51 +6,59 @@ PID.update <- function(series, nfit, nburnin = 100, alpha = 0.1,
   T <- length(y)
   
   # Check the series length
-  if (T < (nfit+nburnin+1))
+  if (T < (nfit + nburnin + 1))
     stop("series length must be larger than nfit+nburnin")
   
   # Initialize data storage variables
-  predSeq <- loSeq <- upSeq <- rep(0, T-nfit)
-  scores <- errs <- qs <- qts <- integrators <- scorecasts <- rep(0, T-nfit)
+  predSeq <- loSeq <- upSeq <- rep(0, T - nfit)
+  scores <- errs <- qs <- qts <- integrators <- scorecasts <- rep(0, T - nfit)
   
-  for (t in (nfit+1):T) {
+  for (t in (nfit + 1):T) {
     # Learning rate
-    t_lr <- t-fit-1
-    t_lr_min <- max(t_lr-nburnin+1, 1)
-    lr_t <- ifelse(t_lr > 0, lr*(max(scores[t_lr_min, t_lr]) - min(scores[t_lr_min, t_lr])), lr)
+    t_lr <- t - nfit
+    t_lr_min <- max(t_lr - nburnin, 1)
+    lr_t <- ifelse(t_lr > 1, lr * (max(scores[t_lr_min:(t_lr - 1)]) - min(scores[t_lr_min:(t_lr - 1)])), lr)
     
     # Fit AR model and get point forecasts
-    out <- forecast::Arima(y[(t-nfit):(t-1)], order = c(2,0,0),
+    out <- forecast::Arima(y[t_lr:(t-1)], # use rolling window (length = nfit) for AR model training
+                           order = c(2,0,0),
                            include.mean = TRUE, method = "CSS")
     fc <- forecast::forecast(out, h = 1)
     pred <- as.vector(fc$mean)
     
     # Observe y_t, calculate conformity score and err
-    scores[t-nfit] <- abs(y[t] - pred)
-    errs[t-nfit] <- !(scores[t-nfit] <= qs[t-nfit])
+    scores[t_lr] <- abs(y[t] - pred)
+    errs[t_lr] <- !(qs[t_lr] >= scores[t_lr])
     
     # Calculate saturation function
-    integrator_arg <- ifelse(t-fit-1 > 0, sum(errs[1:(t-fit-1)]) - (t-fit-1)*alpha, 0)
-    integrator <- saturation_fn_log(integrator_arg, t-fit, Csat, KI)
+    integrator_arg <- ifelse(t_lr - 1 > 0, sum(errs[1:(t_lr - 1)]) - (t_lr - 1) * alpha, 0)
+    integrator <- saturation_fn_log(integrator_arg, t_lr, Csat, KI)
     
     # Train scorecaster if necessary
-    if (scorecast & ((t-nfit-1) > nburnin)) {
-      curr_scores <- scores[1:(t-nfit-1)]
+    if (scorecast && (nburnin <= t_lr) && (t_lr < (T - nfit))) {
+      curr_scores <- scores[1:t_lr] # use expanding window for scorecaster
       model <- forecast::thetaf(curr_scores, h = 1)
-      scorecasts[t-nfit] <- as.numeric(model$mean)
+      scorecasts[t_lr + 1] <- as.numeric(model$mean)
     }
     
     # Update the next quantile
-    if (t < T) {
-      grad <- ifelse(errs[t-nfit], -(1-alpha), alpha)
-      qts[t-nfit+1] <- qts[t-nfit] - lr_t * grad
-      integrators[t-nfit+1] <- ifelse(integrate, integrator, 0)
-      qs[t-nfit+1] <- qts[t-nfit+1] + integrators[t-nfit+1]
-      if (scorecast) qs[t-nfit+1] <- qs[t-nfit+1] + scorecasts[t-nfit+1]
+    if (t <= (T - 1)) {
+      grad <- ifelse(errs[t_lr], -(1-alpha), alpha)
+      qts[t_lr + 1] <- qts[t_lr] - lr_t * grad # set q_1 = 0
+      integrators[t_lr + 1] <- ifelse(integrate, integrator, 0)
+      qs[t_lr + 1] <- qts[t_lr + 1] + integrators[t_lr + 1]
+      if (scorecast) qs[t_lr + 1] <- qs[t_lr + 1] + scorecasts[t_lr + 1]
     }
+    
+    # Get forecasts and PIs
+    lo <- pred - qs[t_lr]
+    up <- pred + qs[t_lr]
+    
+    predSeq[t_lr] <- pred
+    loSeq[t_lr] <- min(lo, up)
+    upSeq[t_lr] <- max(lo, up)
   }
-  
-  return(qs)
+  return(list(pred = predSeq, lo = loSeq, up = upSeq))
 }
 
 saturation_fn_log <- function(x, t, Csat, KI) {
