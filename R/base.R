548b49b41d8f93a3044c0e36259a239076b11fcf
xqnwang <wang_study1995@163.com>
2024-01-13 22:46:43 +1100

    Include comments for the initially used weighted.quantile function

--- R/base.R
+++ R/base.R
@@ -1,3 +1,4 @@
+# The method commonly used in CP papers, which corresponds to Type 1 in Hyndman & Fan (1996).
 weighted.quantile <- function(v, prob, w = NULL, sorted = FALSE) {
   if (is.null(w)) w <- rep(1, length(v))
   if (!sorted) { o <- order(v); v <- v[o]; w <- w[o] }
