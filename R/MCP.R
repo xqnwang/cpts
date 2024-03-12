a82a2124c18498c9973b3f787cecf757d8e93629
xqnwang <wang_study1995@163.com>
2024-03-13 02:34:30 +1100

    Fix errors in MCP

--- R/MCP.R
+++ R/MCP.R
@@ -55,68 +55,84 @@ MCP <- function(object, alpha = 1 - 0.01 * object$level,
     qt_lower <- qt_upper <- zeromatrix
     
     for (t in indx) {
-      errors_subset <- subset(
-        errors,
-        start = ifelse(!rolling, first_non_na, t - ncal + 1L),
-        end = t)
       
       for (h in seq(horizon)) {
-        # Calculate err_t
-        errt_lower[[lbl]][t, h] <- (-errors[t, h] > qt_lower[t, h])
-        errt_upper[[lbl]][t, h] <- (errors[t, h] > qt_upper[t, h])
+        t_h <- t + h - 1L
+        init_h <- indx[1] + h - 1L
+        first_non_na_h <- first_non_na + h - 1L
         
-        # Calculate saturation function using err_i, i = indx[1],...,t-1
+        if (t_h > last_non_na){
+          break
+        }
+        
+        errors_subset <- subset(
+          errors,
+          start = ifelse(!rolling, first_non_na_h, t_h - ncal + 1L),
+          end = t_h)
+        
+        # Calculate err_t
+        errt_lower[[lbl]][t_h, h] <- (-errors[t_h, h] > qt_lower[t_h, h])
+        errt_upper[[lbl]][t_h, h] <- (errors[t_h, h] > qt_upper[t_h, h])
+        
+        # Calculate saturation function using err_i, i = init_h,...,t_h-1
         if (integrate) {
-          integrator_arg_lower <- ifelse(t > indx[1],
-                                         sum(errt_lower[[lbl]][indx[1]:(t-1), h]) -
-                                           (t - indx[1]) * alpha[i]/2,
+          integrator_arg_lower <- ifelse(t_h > init_h,
+                                         sum(errt_lower[[lbl]][init_h:(t_h-1), h]) -
+                                           (t_h - init_h) * alpha[i]/2,
                                          0)
-          integrator_arg_upper <- ifelse(t > indx[1],
-                                         sum(errt_upper[[lbl]][indx[1]:(t-1), h]) -
-                                           (t - indx[1]) * alpha[i]/2,
+          integrator_arg_upper <- ifelse(t_h > init_h,
+                                         sum(errt_upper[[lbl]][init_h:(t_h-1), h]) -
+                                           (t_h - init_h) * alpha[i]/2,
                                          0)
-          integrator_lower[[lbl]][t, h] <- saturation_fn_log(integrator_arg_lower, t-indx[1]+1, Csat, KI)
-          integrator_upper[[lbl]][t, h] <- saturation_fn_log(integrator_arg_upper, t-indx[1]+1, Csat, KI)
+          integrator_lower[[lbl]][t_h, h] <- saturation_fn_log(integrator_arg_lower, t_h-init_h+1, Csat, KI)
+          integrator_upper[[lbl]][t_h, h] <- saturation_fn_log(integrator_arg_upper, t_h-init_h+1, Csat, KI)
         }
         
         # Train scorecaster (same for different alphas)
         if (scorecast) {
           if (h == 1) {
             model <- forecast::meanf(errors_subset[, h], h = 1)
-            scorecaster_upper[t, h] <- as.numeric(model$mean)
-            scorecaster_lower[t, h] <- -scorecaster_upper[t, h]
+            scorecaster_upper[t_h, h] <- as.numeric(model$mean)
+            scorecaster_lower[t_h, h] <- -scorecaster_upper[t_h, h]
             
           } else {
             model_MA <- forecast::Arima(errors_subset[, h], order = c(0, 0, h-1)) |>
               forecast::forecast(h = 1)
             model_LR <- lm(as.formula(paste0("V", h, " ~ .")), 
-                           data = setNames(as.data.frame(errors_subset[, 1:h]), 
-                                           paste0("V", 1:h))) |>
-              forecast::forecast(newdata = setNames(data.frame(scorecaster_upper[t, 1:(h-1)] |> 
-                                                                 matrix(nrow = 1)),
-                                                    paste0("V", 1:(h-1))))
-            scorecaster_upper[t, h] <- (as.numeric(model_MA$mean) + as.numeric(model_LR$mean))/2
-            scorecaster_lower[t, h] <- -scorecaster_upper[t, h]
+                           data = setNames(
+                             as.data.frame(sapply(1:h, function(j) {
+                               subset(
+                                 errors,
+                                 start = ifelse(!rolling, first_non_na_h-h+j, t_h-h+j-ncal+1L),
+                                 end = t_h-h+j)
+                             })),
+                             paste0("V", 1:h))) |>
+              forecast::forecast(
+                newdata = setNames(data.frame(sapply(1:(h-1), function(j) scorecaster_upper[t_h-h+j, j]) |>
+                                                matrix(nrow = 1)),
+                                   paste0("V", 1:(h-1))))
+            scorecaster_upper[t_h, h] <- (as.numeric(model_MA$mean) + as.numeric(model_LR$mean))/2
+            scorecaster_lower[t_h, h] <- -scorecaster_upper[t_h, h]
           }
         }
         
         # Learning rate (same for the upper and lower bounds)
         if (i == 1)
-          lrt[t, h] <- ifelse(t == indx[1], lr,
+          lrt[t_h, h] <- ifelse(t_h == init_h, lr,
                               lr*(max(errors_subset[, h]) - min(errors_subset[, h])))
         
         # Update the next quantile
-        grad_lower <- ifelse(errt_lower[[lbl]][t, h], -(1-alpha[i]/2), alpha[i]/2)
-        grad_upper <- ifelse(errt_upper[[lbl]][t, h], -(1-alpha[i]/2), alpha[i]/2)
-        qt_lower[t+1, h] <- qt_lower[t, h] - lrt[t, h] * grad_lower +
-          ifelse(integrate, integrator_lower[[lbl]][t, h], 0) +
-          ifelse(scorecast, scorecaster_lower[t, h], 0)
-        qt_upper[t+1, h] <- qt_upper[t, h] - lrt[t, h] * grad_upper +
-          ifelse(integrate, integrator_upper[[lbl]][t, h], 0) +
-          ifelse(scorecast, scorecaster_upper[t, h], 0)
+        grad_lower <- ifelse(errt_lower[[lbl]][t_h, h], -(1-alpha[i]/2), alpha[i]/2)
+        grad_upper <- ifelse(errt_upper[[lbl]][t_h, h], -(1-alpha[i]/2), alpha[i]/2)
+        qt_lower[t_h+1, h] <- qt_lower[t_h, h] - lrt[t_h, h] * grad_lower +
+          ifelse(integrate, integrator_lower[[lbl]][t_h, h], 0) +
+          ifelse(scorecast, scorecaster_lower[t_h, h], 0)
+        qt_upper[t_h+1, h] <- qt_upper[t_h, h] - lrt[t_h, h] * grad_upper +
+          ifelse(integrate, integrator_upper[[lbl]][t_h, h], 0) +
+          ifelse(scorecast, scorecaster_upper[t_h, h], 0)
         # PIs
-        out$lower[[paste0(level[i], "%")]][t+1, h] <- out$mean[t+1, h] - qt_lower[t+1, h]
-        out$upper[[paste0(level[i], "%")]][t+1, h] <- out$mean[t+1, h] + qt_upper[t+1, h]
+        out$lower[[paste0(level[i], "%")]][t_h+1, h] <- out$mean[t_h+1, h] - qt_lower[t_h+1, h]
+        out$upper[[paste0(level[i], "%")]][t_h+1, h] <- out$mean[t_h+1, h] + qt_upper[t_h+1, h]
       }
     }
   }
@@ -124,24 +140,15 @@ MCP <- function(object, alpha = 1 - 0.01 * object$level,
     out$lower <- lapply(out$lower, function(lo) lo[, 1L])
     out$upper <- lapply(upper$lower, function(up) up[, 1L])
     lrt <- lrt[, 1L]
-    if (symmetric) {
-      integrator <- lapply(integrator, function(integrator_t) integrator_t[, 1L])
-      scorecaster <- scorecaster[, 1L]
-    } else {
-      integrator_lower <- lapply(integrator_lower, function(integrator_lower_t) integrator_lower_t[, 1L])
-      integrator_upper <- lapply(integrator_upper, function(integrator_upper_t) integrator_upper_t[, 1L])
-      scorecaster_lower <- scorecaster_lower[, 1L]
-      scorecaster_upper <- scorecaster_upper[, 1L]
-    }
+    integrator_lower <- lapply(integrator_lower, function(integrator_lower_t) integrator_lower_t[, 1L])
+    integrator_upper <- lapply(integrator_upper, function(integrator_upper_t) integrator_upper_t[, 1L])
+    scorecaster_lower <- scorecaster_lower[, 1L]
+    scorecaster_upper <- scorecaster_upper[, 1L]
   }
   out$method <- paste("MCP")
   out$model$lr <- lrt
-  if (symmetric) {
-    out$model$integrator <- list(integrator = integrator)
-    out$model$scorecaster <- list(scorecaster = scorecaster)
-  } else {
-    out$model$integrator <- list(integrator_lower = integrator_lower, integrator_upper = integrator_upper)
-    out$model$scorecaster <- list(scorecaster_lower = scorecaster_lower, scorecaster_upper = scorecaster_upper)
-  }
+  out$model$integrator <- list(integrator_lower = integrator_lower, integrator_upper = integrator_upper)
+  out$model$scorecaster <- list(scorecaster_lower = scorecaster_lower, scorecaster_upper = scorecaster_upper)
+  
   return(structure(out, class = "CPforecast"))
 }
