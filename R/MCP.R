144401e63b9dc2e51e9af16f5bd98ab232f4eba3
xqnwang <wang_study1995@163.com>
2024-03-20 00:38:31 +1100

    Optimize the MCP fun, add print.MCP and help document

--- R/MCP.R
+++ R/MCP.R
@@ -1,19 +1,101 @@
-# Conformal forecasting using Multistep-ahead conformal prediction (MCP)
-# This method can only deal with asymmetric conformity scores, i.e., forecast errors.
+#' Multistep-ahead conformal prediction
+#' 
+#' \code{MCP} computes prediction intervals and other information obtained by
+#' applying the multistep-ahead conformal prediction method. The method can only
+#' deal with asymmetric conformity scores, i.e., forecast errors.
+#' 
+#' @param object An object of class "\code{CVforecast}". It must have an argument
+#' \code{x} for original univariate time series, an argument \code{mean} for
+#' point forecasts and \code{errors} for forecast errors. See the results of a call
+#' to \code{\link{CVforecast}}.
+#' @param alpha A numeric vector of target levels \eqn{1-\alpha}.
+#' @param ncal Length of the calibration set. If \code{rolling=FALSE}, it denotes
+#' the initial period of calibration sets. If \code{rolling=TRUE}, it indicates
+#' the period of each rolling calibration set.
+#' @param rolling If \code{TRUE}, a rolling window strategy will be used to
+#' generate the calibration set for learning rate update and scorecasting.
+#' Otherwise, expanding window will be used.
+#' @param integrate If \code{TRUE}, error integration will be included in the
+#' update process.
+#' @param scorecast If \code{TRUE}, scorecasting will be included in the update
+#' process as a forecast combination of a MA(h-1) model and a linear regression model.
+#' @param lr Initial learning rate used for quantile tracking.
+#' @param Tg The time set to achieve the target absolute guarantee before this.
+#' @param delta The target absolute guarantee is \eqn{1-\alpha-\delta} coverage.
+#' @param Csat A positive constant ensuring that by time \code{Tg}, an absolute
+#' guarantee is of at least \eqn{1-\alpha-\delta} coverage.
+#' @param KI A positive constant to place the integrator on the same scale as the scores.
+#' @return An object of class "\code{CPforecast}".
+#' 
+#' An object of class "\code{CPforecast}" is a list containing the following elements:
+#' \item{x}{The original time series.}
+#' \item{method}{The name of the conformal prediction method as a character string.}
+#' \item{mean}{Point forecasts as a time series for \eqn{h=1}. For \eqn{h>1},
+#' they are returned as a time series matrix with the \eqn{h}th column
+#' containing point forecasts for forecast horizon \eqn{h}. The time index
+#' corresponds to the period for which the forecast is generated.}
+#' \item{errors}{Forecast errors given by
+#' \eqn{e_{t+h} = y_{t+h}-\hat{y}_{t+h|t}}{e[t+h] = y[t+h]-f[t+h]}.}
+#' \item{lower}{A list containing lower limits for prediction intervals for
+#' each \code{level}.}
+#' \item{upper}{A list containing upper limits for prediction intervals for
+#' each \code{level}.}
+#' \item{level}{The confidence values associated with the prediction intervals.}
+#' \item{model}{A list containing information about the conformal prediction model.}
+#' @author Xiaoqian Wang
+#' @examples
+#' 
+#' library(forecast)
+#' 
+#' # Simulation series AR(2)
+#' set.seed(0)
+#' series <- arima.sim(n = 1000, list(ar = c(0.8, -0.5)), sd = sqrt(1))
+#' series <- as.numeric(series)
+#' 
+#' # Setup
+#' far2 <- function(x, h, level) {
+#'   Arima(x, order = c(2, 0, 0)) |> 
+#'     forecast(h = h, level)
+#' }
+#' 
+#' # Cross-validation forecasting
+#' fc <- CVforecast(series, forecastfun = far2, h = 3, level = c(80, 95),
+#'                  forward = TRUE, window = 100, initial = 1)
+#' 
+#' # PID setup
+#' Tg <- 1000
+#' delta <- 0.01
+#' lr <- 0.1
+#' KI <- 2
+#' Csat <- 2 / pi * (ceiling(log(Tg) * delta) - 1 / log(Tg))
+#' 
+#' # MCP with integrator and scorecaster
+#' mcpfc <- MCP(fc, ncal = 100, rolling = TRUE,
+#'              integrate = TRUE, scorecast = TRUE,
+#'              lr = lr, KI = KI, Csat = Csat)
+#' 
+#' @export
 MCP <- function(object, alpha = 1 - 0.01 * object$level,
                 ncal = 10, rolling = FALSE,
                 integrate = TRUE, scorecast = TRUE,
-                lr = 0.1, Tg = length(object$x),
-                Csat = 2 / pi * (ceiling(log(Tg) * 0.01) - 1 / log(Tg)),
+                lr = 0.1, Tg = NULL, delta = NULL,
+                Csat = 2 / pi * (ceiling(log(Tg) * delta) - 1 / log(Tg)),
                 KI = abs(object$errors) |> max(na.rm = TRUE), ...) {
   if (any(alpha >= 1 | alpha <= 0))
     stop("alpha should be in (0, 1)")
-  alpha <- sort(alpha, decreasing = TRUE)
-  level <- 100 * (1 - alpha)
-  
   if (ncal < 10)
     stop("Length of calibration period should at least be 10")
+  if (!is.null(Tg) && !is.null(delta)) {
+    if (!is.null(Csat))
+      warning("Csat is replaced by calculation using Tg and delta")
+    Csat <- 2 / pi * (ceiling(log(Tg) * delta) - 1 / log(Tg))
+  }
   
+  alpha <- sort(alpha, decreasing = TRUE)
+  level <- 100 * (1 - alpha)
+  pf <- ts(as.matrix(object$mean),
+           start = start(object$mean),
+           frequency = frequency(object$mean))
   errors <- ts(as.matrix(object$errors),
                start = start(object$errors),
                frequency = frequency(object$errors))
@@ -23,132 +105,190 @@ MCP <- function(object, alpha = 1 - 0.01 * object$level,
                  start = start(errors),
                  frequency = frequency(errors))
   colnames(namatrix) <- paste0("h=", seq(horizon))
-  zeromatrix <- ts(matrix(0, nrow = NROW(errors), ncol = horizon),
-                   start = start(errors),
-                   frequency = frequency(errors))
-  colnames(zeromatrix) <- paste0("h=", seq(horizon))
+  nalist <- rep(list(namatrix), length(alpha))
+  names(nalist) <- paste0(level, "%")
   
-  lower <- upper <- rep(list(namatrix), length(alpha))
-  names(lower) <- names(upper) <-
-    paste0(level, "%")
-  errt_lower <- errt_upper <-
-    integrator_lower <- integrator_upper <- lower
-  lrt <- scorecaster_lower <- scorecaster_upper <- namatrix
+  lower <- upper <- nalist
+  lrmat <- namatrix
+  if (integrate)
+    integrator_lower <- integrator_upper <- nalist
+  if (scorecast)
+    scorecaster_lower <- scorecaster_upper <- namatrix
   
   out <- list(
-    x = object$x,
-    mean = object$mean,
-    errors = object$errors,
-    lower = lower,
-    upper = upper,
-    level = level
+    x = object$x
   )
   
-  first_non_na <- min(which(!is.na(errors[, 1])))
-  last_non_na <- max(which(!is.na(errors[, 1])))
-  if (last_non_na < first_non_na + ncal - 1L)
-    stop("errors in the input object is not long enough for calibration")
-  indx <- seq(first_non_na + ncal - 1L, last_non_na, by = 1L)
-  
-  for (i in seq(length(alpha))) {
-    lbl <- paste0(level[i], "%")
-    qt_lower <- qt_upper <- zeromatrix
+  for (h in seq(horizon)) {
+    first_non_na <- (!is.na(errors[, h])) |> which() |> min()
+    last_non_na <- (!is.na(errors[, h])) |> which() |> max()
+    if (last_non_na < first_non_na + ncal - 1L)
+      stop("errors in the input object is not long enough for calibration")
+    indx <- seq(first_non_na + ncal - 1L, last_non_na + h - 1L, by = 1L)
     
+    errt_lower_h <- errt_upper_h <-
+      integ_lower_h <- integ_upper_h <-
+      matrix(NA_real_, nrow = NROW(errors), ncol = length(alpha))
+    qts_lower_h <- qts_upper_h <-
+      qs_lower_h <- qs_upper_h <- matrix(0, nrow = NROW(errors), ncol = length(alpha))
     for (t in indx) {
+      errors_subset <- subset(
+        errors[, h],
+        start = ifelse(!rolling, first_non_na, t - ncal + 1L),
+        end = ifelse(t <= last_non_na, t, last_non_na))
       
-      for (h in seq(horizon)) {
-        t_h <- t + h - 1L
-        init_h <- indx[1] + h - 1L
-        first_non_na_h <- first_non_na + h - 1L
-        
-        if (t_h > last_non_na){
-          break
-        }
-        
-        errors_subset <- subset(
-          errors,
-          start = ifelse(!rolling, first_non_na_h, t_h - ncal + 1L),
-          end = t_h)
-        
-        # Calculate err_t
-        errt_lower[[lbl]][t_h, h] <- (-errors[t_h, h] > qt_lower[t_h, h])
-        errt_upper[[lbl]][t_h, h] <- (errors[t_h, h] > qt_upper[t_h, h])
-        
-        # Calculate saturation function using err_i, i = init_h,...,t_h-1
-        if (integrate) {
-          integrator_arg_lower <- ifelse(t_h > init_h,
-                                         sum(errt_lower[[lbl]][init_h:(t_h-1), h]) -
-                                           (t_h - init_h) * alpha[i]/2,
-                                         0)
-          integrator_arg_upper <- ifelse(t_h > init_h,
-                                         sum(errt_upper[[lbl]][init_h:(t_h-1), h]) -
-                                           (t_h - init_h) * alpha[i]/2,
-                                         0)
-          integrator_lower[[lbl]][t_h, h] <- saturation_fn_log(integrator_arg_lower, t_h-init_h+1, Csat, KI)
-          integrator_upper[[lbl]][t_h, h] <- saturation_fn_log(integrator_arg_upper, t_h-init_h+1, Csat, KI)
-        }
-        
-        # Train scorecaster (same for different alphas)
-        if (scorecast) {
-          if (h == 1) {
-            model <- forecast::meanf(errors_subset[, h], h = 1)
-            scorecaster_upper[t_h, h] <- as.numeric(model$mean)
-            scorecaster_lower[t_h, h] <- -scorecaster_upper[t_h, h]
-            
-          } else {
-            model_MA <- forecast::Arima(errors_subset[, h], order = c(0, 0, h-1)) |>
-              forecast::forecast(h = 1)
-            model_LR <- lm(as.formula(paste0("V", h, " ~ .")), 
-                           data = setNames(
-                             as.data.frame(sapply(1:h, function(j) {
-                               subset(
-                                 errors,
-                                 start = ifelse(!rolling, first_non_na_h-h+j, t_h-h+j-ncal+1L),
-                                 end = t_h-h+j)
-                             })),
-                             paste0("V", 1:h))) |>
-              forecast::forecast(
-                newdata = setNames(data.frame(sapply(1:(h-1), function(j) scorecaster_upper[t_h-h+j, j]) |>
-                                                matrix(nrow = 1)),
-                                   paste0("V", 1:(h-1))))
-            scorecaster_upper[t_h, h] <- (as.numeric(model_MA$mean) + as.numeric(model_LR$mean))/2
-            scorecaster_lower[t_h, h] <- -scorecaster_upper[t_h, h]
-          }
-        }
+      if (t <= last_non_na) {
+        # Calculate errt
+        errt_lower_h[t, ] <- (-errors[t, h]) > qs_lower_h[t, ]
+        errt_upper_h[t, ] <- errors[t, h] > qs_upper_h[t, ]
         
         # Learning rate (same for the upper and lower bounds)
-        if (i == 1)
-          lrt[t_h, h] <- ifelse(t_h == init_h, lr,
-                              lr*(max(errors_subset[, h]) - min(errors_subset[, h])))
+        lrmat[t, h] <- ifelse(
+          length(errors_subset) <= 1,
+          lr,
+          lr*(max(errors_subset) - min(errors_subset)))
         
-        # Update the next quantile
-        grad_lower <- ifelse(errt_lower[[lbl]][t_h, h], -(1-alpha[i]/2), alpha[i]/2)
-        grad_upper <- ifelse(errt_upper[[lbl]][t_h, h], -(1-alpha[i]/2), alpha[i]/2)
-        qt_lower[t_h+1, h] <- qt_lower[t_h, h] - lrt[t_h, h] * grad_lower +
-          ifelse(integrate, integrator_lower[[lbl]][t_h, h], 0) +
-          ifelse(scorecast, scorecaster_lower[t_h, h], 0)
-        qt_upper[t_h+1, h] <- qt_upper[t_h, h] - lrt[t_h, h] * grad_upper +
-          ifelse(integrate, integrator_upper[[lbl]][t_h, h], 0) +
-          ifelse(scorecast, scorecaster_upper[t_h, h], 0)
-        # PIs
-        out$lower[[paste0(level[i], "%")]][t_h+1, h] <- out$mean[t_h+1, h] - qt_lower[t_h+1, h]
-        out$upper[[paste0(level[i], "%")]][t_h+1, h] <- out$mean[t_h+1, h] + qt_upper[t_h+1, h]
+        # Update quantile tracking
+        qts_lower_h[t+1, ] <- qts_lower_h[t, ] + lrmat[t, h] * (errt_lower_h[t, ] - alpha/2)
+        qts_upper_h[t+1, ] <- qts_upper_h[t, ] + lrmat[t, h] * (errt_upper_h[t, ] - alpha/2)
+      } else {
+        qts_lower_h[t+1, ] <- qts_lower_h[t, ]
+        qts_upper_h[t+1, ] <- qts_upper_h[t, ]
+      }
+      
+      # Update integrator
+      if (integrate) {
+        el <- errt_lower_h[indx[1]:(ifelse(t <= last_non_na, t, last_non_na)), ] |>
+          matrix(ncol = length(alpha))
+        integrator_lower_arg <- apply(el, 2, sum) - NROW(el)*alpha/2
+        integ_lower_h[t+1, ] <- sapply(
+          1:length(alpha),
+          function(i) saturation_fn_log(integrator_lower_arg[i], NROW(el), Csat, KI))
+        
+        eu <- errt_upper_h[indx[1]:(ifelse(t <= last_non_na, t, last_non_na)), ] |>
+          matrix(ncol = length(alpha))
+        integrator_upper_arg <- apply(eu, 2, sum) - NROW(eu)*alpha/2
+        integ_upper_h[t+1, ] <- sapply(
+          1:length(alpha),
+          function(i) saturation_fn_log(integrator_upper_arg[i], NROW(eu), Csat, KI))
+      }
+      
+      # Update scorecaster
+      if (scorecast) {
+        if (h == 1) {
+          model <- forecast::meanf(errors_subset, h = 1)
+          scorecaster_lower[t+1, h] <- -as.numeric(model$mean)
+          scorecaster_upper[t+1, h] <- as.numeric(model$mean)
+        } else {
+          model_MA <- forecast::Arima(errors_subset, order = c(0, 0, h-1)) |>
+            forecast::forecast(h = 1)
+          model_LR <- lm(
+            as.formula(paste0("V", h, " ~ .")), 
+            data = setNames(
+              as.data.frame(sapply(1:h, function(j) {
+                subset(
+                  errors[, j],
+                  start = ifelse(!rolling, first_non_na-h+j, t-ncal+1-h+j),
+                  end = t-h+j)
+              })),
+              paste0("V", 1:h))
+          ) |>
+            forecast::forecast(
+              newdata = setNames(
+                data.frame(sapply(1:(h-1),
+                                  function(j) scorecaster_upper[t-h+1+j, j]) |> matrix(nrow = 1)),
+                paste0("V", 1:(h-1))))
+          scorecaster_lower[t+1, h] <- -(as.numeric(model_MA$mean) + as.numeric(model_LR$mean))/2
+          scorecaster_upper[t+1, h] <- (as.numeric(model_MA$mean) + as.numeric(model_LR$mean))/2
+        }
+      }
+      
+      # Update the next quantile
+      qs_lower_h[t+1, ] <- qts_lower_h[t+1, ] +
+        ifelse(rep(integrate, length(alpha)), integ_lower_h[t+1, ], rep(0, length(alpha))) +
+        rep(ifelse(scorecast, scorecaster_lower[t+1, h], 0), length(alpha))
+      qs_upper_h[t+1, ] <- qts_upper_h[t+1, ] +
+        ifelse(rep(integrate, length(alpha)), integ_upper_h[t+1, ], rep(0, length(alpha))) +
+        rep(ifelse(scorecast, scorecaster_upper[t+1, h], 0), length(alpha))
+      
+      # PIs
+      for (i in seq(length(alpha))) {
+        lower[[i]][t+1, h] <- pf[t+1, h] - qs_lower_h[t+1, i]
+        upper[[i]][t+1, h] <- pf[t+1, h] + qs_upper_h[t+1, i]
+      }
+    }
+    if (integrate) {
+      for (i in seq(length(alpha))) {
+        integrator_lower[[i]][, h] <- integ_lower_h[, i]
+        integrator_upper[[i]][, h] <- integ_upper_h[, i]
       }
     }
   }
   if (h == 1) {
     out$lower <- lapply(out$lower, function(lo) lo[, 1L])
-    out$upper <- lapply(upper$lower, function(up) up[, 1L])
-    lrt <- lrt[, 1L]
-    integrator_lower <- lapply(integrator_lower, function(integrator_lower_t) integrator_lower_t[, 1L])
-    integrator_upper <- lapply(integrator_upper, function(integrator_upper_t) integrator_upper_t[, 1L])
-    scorecaster_lower <- scorecaster_lower[, 1L]
-    scorecaster_upper <- scorecaster_upper[, 1L]
+    out$upper <- lapply(out$upper, function(up) up[, 1L])
+    lrmat <- lrmat[, 1L]
+    if (integrate) {
+      integrator_lower <- lapply(integrator_lower, function(int_low) int_low[, 1L])
+      integrator_upper <- lapply(integrator_upper, function(int_up) int_up[, 1L])
+    }
+    if (scorecast) {
+      scorecaster_lower <- scorecaster_lower[, 1L]
+      scorecaster_upper <- scorecaster_upper[, 1L]
+    }
   }
   out$method <- paste("MCP")
-  out$model$lr <- lrt
-  out$model$integrator <- list(integrator_lower = integrator_lower, integrator_upper = integrator_upper)
-  out$model$scorecaster <- list(scorecaster_lower = scorecaster_lower, scorecaster_upper = scorecaster_upper)
+  out$mean <- object$mean
+  out$errors <- object$errors
+  out$lower <- lower
+  out$upper <- upper
+  out$level <- level
+  out$model$method <- out$method
+  out$model$call <- match.call()
+  out$model$alpha <- alpha
+  out$model$ncal <- ncal
+  out$model$rolling <- rolling
+  out$model$integrate <- integrate
+  out$model$scorecast <- scorecast
+  out$model$lr <- lr
+  out$model$Tg <- Tg
+  out$model$Csat <- Csat
+  out$model$KI <- KI
+  out$model$lr_update <- lrmat
+  if (integrate)
+    out$model$integrator <- list(lower = integrator_lower, upper = integrator_upper)
+  if (scorecast)
+    out$model$scorecaster <- list(lower = scorecaster_lower, upper = scorecaster_upper)
   
   return(structure(out, class = "CPforecast"))
+}
+
+print.MCP <- function(y, ...) {
+  x <- y$model
+  cat(paste(x$method, "\n\n"))
+  if (!is.null(x$call)) {
+    cat(paste("Call:\n"))
+    for (i in 1:length(deparse(x$call))) {
+      cat(paste("", deparse(x$call)[i]), "\n")
+    }
+    cat(paste("\n"))
+  }
+  
+  cat("  Conformal prediction settings:\n")
+  cat(paste("    symmetric = FALSE\n"))
+  cat(paste("    rolling =", x$rolling, "\n"))
+  cat(paste("   ", ifelse(rolling, "window =", "initial window ="), x$ncal, "\n"))
+  
+  cat("\n  Quantile tracking parameters:\n")
+  cat(paste("    lr =", x$lr, "\n"))
+  
+  if (x$integrate) {
+    cat("\n  Error integration parameters:\n")
+    cat(paste("    KI =", round(x$KI, 3), "\n"))
+    cat(paste("    Csat =", round(x$Csat, 3), "\n"))
+  }
+  
+  if (x$scorecast) {
+    cat("\n  Scorecaster is used.\n")
+  }
 }
