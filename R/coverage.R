cded6fe80c537b7f5c7acb0e018b54394ec7db87
xqnwang <wang_study1995@163.com>
2024-03-26 23:31:03 +1100

    Only consider one level for coverage and width calculation

--- R/coverage.R
+++ R/coverage.R
@@ -1,25 +1,30 @@
 #' @importFrom stats window
 #' @import zoo rollmean
 #' @export
-coverage <- function(object, level = object$level, window = NULL, na.rm = TRUE) {
+coverage <- function(object, ..., level = 95, window = NULL, na.rm = TRUE) {
   # Check inputs
-  if (any(!(c("x", "LOWER", "UPPER") %in% names(object))))
-    stop("x, LOWER, and UPPER are required for coverage calculation")
-  if (!(is.list(object$LOWER) && is.list(object$UPPER)))
-    stop("LOWER and UPPER should be a list")
-  if (all(level > 0 & level < 1)) {
+  if (level > 0 && level < 1) {
     level <- 100 * level
-  } else if (any(level < 0 | level > 99.99)) {
+  } else if (level < 0 || level > 99.99) {
     stop("confidence limit out of range")
   }
-  
-  # Extract information of interest
-  level <- sort(level)
-  levelname <- paste0(level, "%")
-  lower <- object$LOWER[levelname]
-  upper <- object$UPPER[levelname]
-  horizon <- ncol(lower[[1]])
-  
+  dots <- rlang::dots_list(...)
+  if (missing(object)) {
+    if (any(!(c("x", "LOWER", "UPPER") %in% names(dots))))
+      stop("x, LOWER, and UPPER are required for coverage calculation")
+  } else {
+    if (any(!(c("x", "LOWER", "UPPER") %in% names(object))))
+      stop("x, LOWER, and UPPER are required for coverage calculation")
+    if (!(level %in% object$level))
+      stop("no interval forecasts of target confidence level in object")
+    levelname <- paste0(level, "%")
+    x <- object$x
+    LOWER <- object$LOWER[[levelname]]
+    UPPER <- object$UPPER[[levelname]]
+  }
+  lower <- LOWER
+  upper <- UPPER
+  horizon <- ncol(lower)
   period <- frequency(object$x)
   x <- ts(matrix(rep(object$x, horizon), ncol = horizon, byrow = FALSE),
           start = start(object$x),
@@ -27,40 +32,30 @@ coverage <- function(object, level = object$level, window = NULL, na.rm = TRUE) 
   
   # Match time
   tspx <- tsp(x)
-  tspl <- tsp(lower[[1]])
-  tspu <- tsp(upper[[1]])
+  tspl <- tsp(lower)
+  tspu <- tsp(upper)
   start <- max(tspx[1], tspl[1], tspu[1])
   end <- min(tspx[2], tspl[2], tspu[2])
   
   x <- window(x, start = start, end = end)
-  lower <- lapply(lower, function(lo) window(lo, start = start, end = end))
-  upper <- lapply(upper, function(up) window(up, start = start, end = end))
+  lower <- window(lower, start = start, end = end)
+  upper <- window(upper, start = start, end = end)
   n <- nrow(x)
   
   # If coverage matrix
-  covmat <- `names<-` (lapply(levelname, function(i) {
-    lo <- lower[[i]]
-    up <- upper[[i]]
-    xx <- x
-    cov <- (lo <= xx & xx <= up)
-    cov <- ts(cov, start = start, end = end, frequency = period)
-    colnames(cov) <- colnames(lo)
-    return(cov)
-  }), levelname)
+  covmat <- (lower <= x & x <= upper) |>
+    ts(start = start, end = end, frequency = period)
+  colnames(covmat) <- colnames(lower)
   
   # Mean coverage
-  covmean <- sapply(covmat, function(cov) {
-    apply(cov, 2, mean, na.rm = na.rm)
-  })
+  covmean <- apply(covmat, 2, mean, na.rm = na.rm)
   
   # Rolling mean coverage
   if (!is.null(window)) {
     if (window >= n)
       stop("the `window` argument should be smaller than the total period of interest")
-    covrmean <- lapply(covmat, function(cov) {
-      apply(cov, 2, zoo::rollmean, k = window, na.rm = na.rm) |>
-        ts(end = end, frequency = period)
-    })
+    covrmean <- apply(covmat, 2, zoo::rollmean, k = window, na.rm = na.rm) |>
+      ts(end = end, frequency = period)
   }
   
   out <- list(
--- R/width.R
+++ R/width.R
@@ -1,55 +1,53 @@
 #' @export
-width <- function(object, level = object$level, window = NULL, na.rm = TRUE) {
+width <- function(object, ..., level = 95, window = NULL, na.rm = TRUE) {
   # Check inputs
-  if (any(!(c("LOWER", "UPPER") %in% names(object))))
-    stop("LOWER, and UPPER are required for coverage calculation")
-  if (!(is.list(object$LOWER) && is.list(object$UPPER)))
-    stop("LOWER and UPPER should be a list")
-  if (all(level > 0 & level < 1)) {
+  if (level > 0 && level < 1) {
     level <- 100 * level
-  } else if (any(level < 0 | level > 99.99)) {
+  } else if (level < 0 || level > 99.99) {
     stop("confidence limit out of range")
   }
-  
-  # Extract information of interest
-  level <- sort(level)
-  levelname <- paste0(level, "%")
-  lower <- object$LOWER[levelname]
-  upper <- object$UPPER[levelname]
-  horizon <- ncol(lower[[1]])
-  period <- frequency(lower[[1]])
+  dots <- rlang::dots_list(...)
+  if (missing(object)) {
+    if (any(!(c("LOWER", "UPPER") %in% names(dots))))
+      stop("LOWER, and UPPER are required for coverage calculation")
+  } else {
+    if (any(!(c("LOWER", "UPPER") %in% names(object))))
+      stop("LOWER, and UPPER are required for coverage calculation")
+    if (!(level %in% object$level))
+      stop("no interval forecasts of target confidence level in object")
+    levelname <- paste0(level, "%")
+    LOWER <- object$LOWER[[levelname]]
+    UPPER <- object$UPPER[[levelname]]
+  }
+  lower <- LOWER
+  upper <- UPPER
+  horizon <- ncol(lower)
+  period <- frequency(lower)
   
   # Match time
-  tspl <- tsp(lower[[1]])
-  tspu <- tsp(upper[[1]])
+  tspl <- tsp(lower)
+  tspu <- tsp(upper)
   start <- max(tspl[1], tspu[1])
   end <- min(tspl[2], tspu[2])
   
-  lower <- lapply(lower, function(lo) window(lo, start = start, end = end))
-  upper <- lapply(upper, function(up) window(up, start = start, end = end))
-  n <- nrow(lower[[1]])
+  lower <- window(lower, start = start, end = end)
+  upper <- window(upper, start = start, end = end)
+  n <- nrow(lower)
   
   # Width matrix
-  widmat <- `names<-` (lapply(levelname, function(i) {
-    wid <- upper[[i]] - lower[[i]]
-    wid <- ts(wid, start = start, end = end, frequency = period)
-    colnames(wid) <- colnames(upper[[i]])
-    return(wid)
-  }), levelname)
+  widmat <- (upper- lower) |>
+    ts(start = start, end = end, frequency = period)
+  colnames(widmat) <- colnames(lower)
   
   # Mean coverage
-  widmean <- sapply(widmat, function(wid) {
-    apply(wid, 2, mean, na.rm = na.rm)
-  })
+  widmean <- apply(widmat, 2, mean, na.rm = na.rm)
   
   # Rolling mean coverage
   if (!is.null(window)) {
     if (window >= n)
       stop("the `window` argument should be smaller than the total period of interest")
-    widrmean <- lapply(widmat, function(wid) {
-      apply(wid, 2, zoo::rollmean, k = window, na.rm = na.rm) |>
+    widrmean <- apply(widmat, 2, zoo::rollmean, k = window, na.rm = na.rm) |>
         ts(end = end, frequency = period)
-    })
   }
   
   out <- list(
