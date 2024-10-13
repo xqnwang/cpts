5f971b3239eeb061112980cf1c1b01e003cb77d2
xqnwang <wang_study1995@163.com>
2024-10-13 22:23:21 +1100

    Clean up docs

--- R/base.R
+++
@@ -1,8 +0,0 @@
-# The method commonly used in CP papers, which corresponds to Type 1 in Hyndman & Fan (1996).
-weighted.quantile <- function(v, prob, w = NULL, sorted = FALSE) {
-  if (is.null(w)) w <- rep(1, length(v))
-  if (!sorted) { o <- order(v); v <- v[o]; w <- w[o] }
-  i <- which(cumsum(w/sum(w)) >= prob)
-  if (length(i) == 0) return(Inf) # Can happen with infinite weights
-  else return(v[min(i)])
-}
